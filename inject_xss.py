2b5ea8709a6cfd15d22eba82fa207a972fb898e0
ShogunExecutioner <ShogunExecutioner@users.noreply.github.com>
2020-07-01 01:54:59 +0200

    - Merge with github - Added halo for friendly looking live status updates

--- inject_xss.py
+++ inject_xss.py
@@ -3,10 +3,33 @@ class InjectXSS:
         pass
 
 
+def distribute(target_domain: str):
+    try:
+        with open(f'output/{target_domain}.txt') as file:
+            url_list = list(set(map(str.strip, file.readlines())))
+    except (IOError, FileNotFoundError):
+        logging.error("Output file not found")
+    cores = multiprocessing.cpu_count()
+    if cores == 1:
+        p = multiprocessing.Process(target=InjectXSS, args=(list, 0,))
+        p.start()
+        p.join()
+    else:
+        ps = []
+        chunk = int(limit / instances)
+        for i in range(0, len(list), chunk):
+            ps.append(multiprocessing.Process(target=InjectXSS, args=(list[i:i + chunk], int(i / chunk),)))
+        for p in ps:
+            p.start()
+        for p in ps:
+            p.join()
+
+
 if __name__ == '__main__':
     import os
     import sys
     import multiprocessing
+    import logging
     import fetch_and_filter
     import config
     import helpers
@@ -14,5 +37,16 @@ if __name__ == '__main__':
     try:
         import aiohttp
     except ModuleNotFoundError:
-        helpers.error("Please install required modules")
-    cores = multiprocessing.cpu_count()
+        helpers.failure("Missing package: aiohttp")
+    try:
+        sys.argv.append("oktob.io")  # TODO delete this line
+        target_domain = sys.argv[1]
+        helpers.check_target_domain(target_domain)
+        helpers.create_log_file(target_domain)
+    except IndexError:
+        helpers.usage_msg()
+    try:
+        # distribute(target_domain)
+        helpers.success("All Done!")
+    except KeyboardInterrupt:
+        helpers.failure("Interrupted by user")
