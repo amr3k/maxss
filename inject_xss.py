e2e3f483e944d511f0b7a95a3e178ef30f8e0cae
ShogunExecutioner <ShogunExecutioner@users.noreply.github.com>
2020-07-02 01:58:57 +0200

    - Fixed some bugs but still some exist

--- inject_xss.py
+++ inject_xss.py
@@ -9,7 +9,7 @@ class InjectXSS:
             self.headers = config.headers
             self.timeout = aiohttp.ClientTimeout(total=config.timeout)
         except Exception as e:
-            helpers.failure(f"Check config.py \n{e.__str__()}")
+            failure(f"Check config.py \n{e.__str__()}")
 
         loop = asyncio.get_event_loop()
         future = asyncio.ensure_future(self.run())
@@ -31,67 +31,41 @@ class InjectXSS:
     async def new_request(self, url, session):
         try:
             async with session.get(url) as response:
-                helpers.request_counter(url)
+                request_counter(url)
                 return response
         except Exception as e:
-            helpers.failure(e.__str__())
+            failure(e.__str__())
 
     async def on_request_start(self, session, trace_config_ctx, params):
         pass
 
     async def on_request_finish(self, session, trace_config_ctx, params):
-        helpers.request_counter(params.url)
-
-
-def distribute(urls_file: str):
-    try:
-        with open(urls_file) as file:
-            url_list = list(set(map(str.strip, file.readlines())))
-    except (IOError, FileNotFoundError):
-        helpers.failure("Output file not found")
-    total_urls = len(url_list)
-    helpers.TOTAL_URLS = total_urls
-    cores = multiprocessing.cpu_count()
-    if cores == 1:
-        p = multiprocessing.Process(target=InjectXSS, args=(url_list,))
-        p.start()
-        p.join()
-    else:
-        process_list = []
-        for i in range(cores):
-            chunk_list = url_list[i * total_urls // cores:(i + 1) * total_urls // cores]
-            process_list.append(
-                multiprocessing.Process(target=InjectXSS, args=(chunk_list,)))
-        for p in process_list:
-            p.start()
-        for p in process_list:
-            p.join()
+        request_counter(params.url)
 
 
 if __name__ == '__main__':
-    import os
-    import sys
-    import multiprocessing
     import asyncio
-    import logging
-    import fetch_and_filter
     import config
-    import helpers
+    from fetch_and_filter import FetchAndFilter
+    from helpers import *
 
     try:
         import aiohttp
     except ModuleNotFoundError:
-        helpers.failure("Missing package: aiohttp")
+        failure("Missing package: aiohttp")
     try:
         sys.argv.append("video.techcrunch.com")  # TODO delete this line
         target_domain = sys.argv[1]
-        helpers.check_target_domain(target_domain)
-        helpers.create_log_file(target_domain)
+        check_target_domain(target_domain)
+        create_log_file(target_domain)
     except IndexError:
-        helpers.usage_msg()
+        usage_msg()
     try:
-        get_urls = fetch_and_filter.FetchAndFilter()
-        distribute(get_urls.file_path)
-        helpers.success("All Done!")
+        get_urls = FetchAndFilter(target_domain=target_domain)
+        with open(get_urls.file_path) as file:
+            url_list = list(set(map(str.strip, file.readlines())))
+        injector = InjectXSS(url_list=url_list)
+        # distribute(get_urls.file_path)
+        success("All Done!")
     except KeyboardInterrupt:
-        helpers.failure("Interrupted by user")
+        failure("Interrupted by user")
