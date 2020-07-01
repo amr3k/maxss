9ac6b95e335b67dba957b7aa06ac9e4f429b3550
Amr <akkk33@protonmail.com>
2020-07-01 07:21:07 +0200

    - Merge with github - Added halo for friendly looking live status updates - Added configuration variables - Implemented main class - Added bunch of user agents

--- config.py
+++ config.py
@@ -1,2 +1,20 @@
-headers = {}
-proxy = {}
+import sys
+from random import choice
+import json
+import helpers
+
+proxy = ""
+maximum_concurrent_connections = 10
+timeout = 3  # Timeout for each request (n seconds)
+xss_hunter_payload = ''
+try:
+    with open(f'{sys.path[0]}/user-agents.json') as file:
+        user_agents = json.load(file)
+except Exception:
+    helpers.failure("Couldn't load user-agents.json file")
+headers = {
+    'User-Agent': f"{choice(user_agents)}{xss_hunter_payload}"
+}
+
+if __name__ == '__main__':
+    print(headers)
