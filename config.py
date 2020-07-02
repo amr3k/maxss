6f1179dddba3d4a9be4137c00ea8b1fc0d434fd7
Amr <akkk33@protonmail.com>
2020-07-02 03:01:12 +0200

    It's working fine except for the max concurrent connections

--- config.py
+++ config.py
@@ -1,17 +1,23 @@
 import sys
 from random import choice
 import json
-import helpers
+from helpers import *
 
-proxy = ""
-maximum_concurrent_connections = 10
-timeout = 3  # Timeout for each request (n seconds)
-xss_hunter_payload = ''
 try:
-    with open(f'{sys.path[0]}/user-agents.json') as file:
+    with open(f"{sys.path[0]}/static/config.json") as config_file:
+        settings = json.load(config_file)
+
+        proxy = settings['http-proxy']
+        maximum_concurrent_connections = settings['max-connections']
+        timeout = settings['timeout']  # Timeout for each request (n seconds)
+        xss_hunter_payload = settings['xss-payload']
+except Exception:
+    failure("Error parsing config.json file")
+try:
+    with open(f'{sys.path[0]}/static/user-agents.json') as file:
         user_agents = json.load(file)
 except Exception:
-    helpers.failure("Couldn't load user-agents.json file")
+    failure("Couldn't load user-agents.json file")
 headers = {
     'User-Agent': f"{choice(user_agents)}{xss_hunter_payload}"
 }
