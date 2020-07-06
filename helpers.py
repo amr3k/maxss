fd79a013b5779130abb32a3ed740b2f15531b759
ShogunExecutioner <ShogunExecutioner@users.noreply.github.com>
2020-07-07 01:44:32 +0200

    + Now it has simple command line options thanks to click ! Renamed project ! Refactored project files ! Refactored Injector class methods + Added live status updates for various jobs + Added more info in log file + Some bug fixes

--- helpers.py
+++ helpers.py
@@ -5,16 +5,24 @@ import sys
 from datetime import datetime
 from time import time
 
+CURRENT_DIR = sys.path[0]
+CURRENT_TIME = datetime.fromtimestamp(time()).strftime("%Y%m%d%H%M")
 
-def init_halo():
-    try:
-        from halo import Halo
-    except ModuleNotFoundError:
-        sys.exit("Missing package: halo")
+URL_COUNT = 0
+SENT_REQUESTS = 0
+
+SUCCESSFUL_ATTEMPTS = 0
+FAILED_ATTEMPTS = 0
 
 
-init_halo()
-from halo import Halo
+def missing_module():
+    sys.exit("Please install required packages with pip install -r requirements.txt")
+
+
+try:
+    from halo import Halo
+except ModuleNotFoundError:
+    missing_module()
 
 live_status = Halo()
 live_status.start()
@@ -30,29 +38,55 @@ def warning(message: str):
     logging.warning(message)
 
 
+def final_stats():
+    warning(f"Total URLS: {URL_COUNT}")
+    logging.info(f"Total URLS: {URL_COUNT}")
+    warning(f"Successful attempts: {SUCCESSFUL_ATTEMPTS}")
+    logging.info(f"Successful attempts: {SUCCESSFUL_ATTEMPTS}")
+    warning(f"Failed attempts: {FAILED_ATTEMPTS}")
+    logging.info(f"Failed attempts: {FAILED_ATTEMPTS}")
+    try:
+        success_rate = f"{(SUCCESSFUL_ATTEMPTS / URL_COUNT):.0%}"
+    except ZeroDivisionError:
+        success_rate = "0%"
+    warning(f"Success rate = {success_rate}")
+    logging.info(f"Success rate = {success_rate}")
+
+
 def success(message: str):
     live_status.succeed(text=message)
+    final_stats()
     logging.info(message)
 
 
 def failure(message: str):
     live_status.fail(text=message)
     logging.error(message)
+    final_stats()
     sys.exit(1)
 
 
-CURRENT_DIR = sys.path[0]
-CURRENT_TIME = datetime.fromtimestamp(time()).strftime("%Y%m%d%H%M")
-
-URL_COUNT = 0
-SENT_REQUESTS = 0
-
-
-def request_counter(url: str):
+def increase_sent_request_by_1():
     global SENT_REQUESTS
-
     SENT_REQUESTS += 1
-    live_status.text = f"({SENT_REQUESTS}/{URL_COUNT}) {url[:120]}"
+
+
+def failed_request(url):
+    global FAILED_ATTEMPTS
+
+    increase_sent_request_by_1()
+    live_status.text = f"({SENT_REQUESTS}/{URL_COUNT}) Could not reach {url}"
+    FAILED_ATTEMPTS += 1
+    logging.warning(f"Could not reach {url}")
+
+
+def successful_request(url: str):
+    global SUCCESSFUL_ATTEMPTS
+
+    increase_sent_request_by_1()
+    live_status.text = f"({SENT_REQUESTS}/{URL_COUNT}) Sending to {url}"
+    SUCCESSFUL_ATTEMPTS += 1
+    logging.info(f"Sending to {url}")
 
 
 def create_log_file(domain):
@@ -65,7 +99,7 @@ def create_log_file(domain):
 def extension_list() -> list:
     try:
         with open(f"{sys.path[0]}/static/extensions.json") as file:
-            return json.load(file)  # Add more as you like
+            return list(json.load(file))
     except FileNotFoundError:
         failure("Couldn't find extensions.json")
     except Exception as e:
@@ -73,9 +107,40 @@ def extension_list() -> list:
 
 
 def check_target_domain(target_domain):
-    update_status("Checking domain validity")
+    update_status("Validating domain")
     try:
         if not re.match('^[a-z]+([a-z]+.)+[a-z]+$', target_domain):
-            failure("Please type only the target domain like google.com or sub-domain like mail.google.com")
+            failure("Invalid domain! Please type a valid domain name like google.com or mail.google.com")
     except TypeError:
         failure("Invalid domain")
+
+
+def validate_urls(url_list) -> list:
+    update_status("Validating URL list")
+    final_list = []
+    try:
+        for u in url_list:
+            if not u.startswith('http'):
+                u = f'http://{u}'
+            if re.match(
+                    '^(http(s)?):\/\/[(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}([-a-zA-Z0-9@:%_\+.~#?&//=]*)$',
+                    u):
+                final_list.append(re.sub(':[0-9]{2,5}', '', u))
+    except (TypeError, UnicodeError):
+        failure('Failed while examining URL list')
+    return sanitize_urls(final_list)
+
+
+def sanitize_urls(url_list) -> list:
+    """
+    Remove unnecessary links like css, jpg files
+    :return:
+    """
+    update_status("Removing unnecessary links")
+    final_urls = url_list.copy()
+    for url in url_list:
+        url = url.strip()
+        for extension in extension_list():
+            if url.find(f'.{extension}?') > 0 or url.endswith(f'.{extension}'):
+                final_urls.remove(url)
+    return list(filter(None, final_urls))  # To remove empty strings
