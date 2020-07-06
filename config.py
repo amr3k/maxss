e3f77e0e5e59e01b268c1f971cde663dddfabad7
ShogunExecutioner <ShogunExecutioner@users.noreply.github.com>
2020-07-07 01:44:32 +0200

    + Now it has simple command line options thanks to click ! Renamed project ! Refactored project files ! Refactored Injector class methods + Added live status updates for various jobs + Added more info in log file + Some bug fixes

--- config.py
+++ config.py
@@ -1,23 +1,24 @@
+import json
 import sys
 from random import choice
-import json
-from helpers import *
+
+import helpers
 
 try:
     with open(f"{sys.path[0]}/static/config.json") as config_file:
         settings = json.load(config_file)
 
-        proxy = settings['http-proxy']
+        proxy = settings['http-__proxy']
         maximum_concurrent_connections = settings['max-connections']
-        timeout = settings['timeout']  # Timeout for each request (n seconds)
+        timeout = settings['__timeout']  # Timeout for each request (n seconds)
         xss_hunter_payload = settings['xss-payload']
 except Exception:
-    failure("Error parsing config.json file")
+    helpers.failure("Error parsing config.json file")
 try:
     with open(f'{sys.path[0]}/static/user-agents.json') as file:
         user_agents = json.load(file)
 except Exception:
-    failure("Couldn't load user-agents.json file")
+    helpers.failure("Couldn't load user-agents.json file")
 headers = {
     'User-Agent': f"{choice(user_agents)}{xss_hunter_payload}"
 }
