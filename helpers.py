6d0dd6123516174f0080bb0119ad1ab0d92b5ff3
ShogunExecutioner <ShogunExecutioner@users.noreply.github.com>
2020-07-08 08:55:17 +0200

    + Added basic use of parallelism with multiprocessing. * Altered web archive api link to fetch json with custom filters to save time. * Enhanced imports in web_archive.py + Added my own xss payloads so I don't waste time :)

--- helpers.py
+++ helpers.py
@@ -1,14 +1,15 @@
 import json
 import logging
-import re
-import sys
 from datetime import datetime
+from os import mkdir
 from os.path import sep
+from re import match, sub
+from sys import exit, path
 from time import time
 
 from halo import Halo
 
-CURRENT_DIR = sys.path[0]
+CURRENT_DIR = path[0]
 CURRENT_TIME = datetime.fromtimestamp(time()).strftime("%Y%m%d%H%M")
 LOG_FILE_PATH = ""
 URL_COUNT = 0
@@ -60,7 +61,7 @@ def failure(message: str):
     live_status.fail(text=message)
     logging.error(message)
     final_stats()
-    sys.exit(1)
+    exit(1)
 
 
 def increase_sent_request_by_1():
@@ -90,11 +91,21 @@ def successful_request(url: str):
 
 def create_log_file(domain):
     global LOG_FILE_PATH
-    LOG_FILE_PATH = f"{CURRENT_DIR}{sep}logs{sep}{domain}_{CURRENT_TIME}.log"
-    logging.basicConfig(format="%(asctime)s (%(process)d) [%(filename)s:%(lineno)3d] [%(levelname)s]: %(message)s",
-                        level=logging.INFO,
-                        filename=LOG_FILE_PATH,
-                        filemode="w", datefmt="[%Y-%m-%d %H:%M]")
+    try:
+        LOG_FILE_PATH = f"{CURRENT_DIR}{sep}logs{sep}{domain}_{CURRENT_TIME}.log"
+        logging.basicConfig(format="%(asctime)s (%(process)d) [%(filename)s:%(lineno)3d] [%(levelname)s]: %(message)s",
+                            level=logging.INFO,
+                            filename=LOG_FILE_PATH,
+                            filemode="w", datefmt="[%Y-%m-%d %H:%M]")
+    except (OSError, IOError, BufferError, PermissionError, WindowsError):
+        failure("Cannot create log file, check your submitted file name")
+
+
+def create_output_dir():
+    try:
+        mkdir(f'{path[0]}{sep}output')
+    except FileExistsError:
+        pass
 
 
 def extension_list() -> list:
@@ -110,7 +121,7 @@ def extension_list() -> list:
 def check_target_domain(target_domain):
     update_status("Validating domain")
     try:
-        if not re.match('^[a-z]+([a-z]+.)+[a-z]+$', target_domain):
+        if not match('^[a-z]+([a-z]+.)+[a-z]+$', target_domain):
             failure("Invalid domain! Please type a valid domain name like google.com or mail.google.com")
     except TypeError:
         failure("Invalid domain")
@@ -123,8 +134,8 @@ def validate_urls(url_list) -> list:
         for u in url_list:
             if not u.startswith('http'):
                 u = f'http://{u}'
-            if re.match('^https?://[a-zA-Z0-9@:_]{2,256}\.[a-z]{2,6}[-a-zA-Z0-9@:%_+.~#?&/=]*$', u):
-                final_list.append(re.sub(':[0-9]{2,5}', '', u))
+            if match('^https?://[a-zA-Z0-9@:_]{2,256}\.[a-z]{2,6}[-a-zA-Z0-9@:%_+.~#?&/=]*$', u):
+                final_list.append(sub(':[0-9]{2,5}', '', u))
     except (TypeError, UnicodeError):
         failure('Failed while examining URL list')
     return sanitize_urls(final_list)
